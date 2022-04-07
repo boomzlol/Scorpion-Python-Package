f3fd40ef554529678e8fb24aebdf410949b54968
boomzlol <78658810+boomzlol@users.noreply.github.com>
2022-04-07 04:17:41 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Scorpion-1.0',
+    name='Scorpion',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
